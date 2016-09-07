34f8bb803ccb08ba7c3e9ac4ec647bde9970e814
evelthon <epro@prodromou.eu>
2016-09-07 17:10:44 +0300

    New: Export translation file to XLSX file New: Import an XLSX file into DSpace XMLUI xml file format New: shell options added to program to parse arguments

--- README.md
+++ README.md
@@ -3,12 +3,10 @@
 The aim of this project is to assist in translating the XML UI into 
 another language, other than english.
 
-It allows you to create a new `message_xx.xml` file by combining data
-from `message.xml` and an older `message_xx.xml` file.
-The output of this is an xml language file with all messages keys of
-the latest messages.xml, where all translation from an older 
-messages_xx.xml have been ported over. You are left translating of only
- the newly added keys, without having to check them one by one.
+It allows you to:
+1. Create a new `message_xx.xml` file by combining data from `message.xml` and an older `message_xx.xml` file. The output of this is an xml language file with all messages keys of the latest messages.xml, where all translation from an older messages_xx.xml have been ported over. You are left translating of only the newly added keys, without having to check them one by one.
+2. Convert a translation xml file to XLSX format. This allows for editing translation strings in Office, and give the ability to a non-programmer to do the translation.
+3. Convert the exported XLSX file back to the native xml file for usage by DSpace.
 
 
 ## Usage
@@ -16,4 +14,8 @@ messages_xx.xml have been ported over. You are left translating of only
 and an older version of a translation `message_xx.xml`.
 2. Edit the python script, and update `incomplete_translation_file`
  accordingly.
-3. Execute with `python3 lang-util.py`
+3. Execute with `python3 lang-util.py {migrate | export | import}`
+
+
+###Note
+Use `python3 lang-util.py -h` for help.
--- lang-util.py
+++ lang-util.py
@@ -3,16 +3,24 @@
 
 from lxml import etree
 from openpyxl import Workbook, load_workbook
+from collections import OrderedDict
+import argparse
 
 source_file = 'messages.xml'
 incomplete_translation_file = 'messages_el.xml'
 output_file = 'output.xml'
+imported_file = 'imported.xml'
+exported_file = 'exported.xlsx'
 
+class XmluiParser:
+    def __init__(self, from_file, to_file):
+        self.from_file = from_file
+        self.to_file = to_file
 
-class XmluiParser():
-    def __init__(self):
+    def processFiles(self):
+
         # Parse the non completed translation file
-        doc = etree.parse(incomplete_translation_file)
+        doc = etree.parse(self.from_file)
         di = dict()
         for n in doc.getroot().iterdescendants():
             tmp = n.attrib
@@ -29,8 +37,7 @@ class XmluiParser():
                 di[tmp.get('key')] = n.text
                 # print(str(tmp.get('key')) + " --  "  + str(n.text))
 
-
-        # Parse the new English translation
+        # Parse the latest English translation
         dest = etree.parse(source_file)
         root = dest.getroot()
 
@@ -42,33 +49,37 @@ class XmluiParser():
                 if code:
                     code[0].text = val
 
-        f = open(output_file, 'wb')
+        f = open(self.to_file, 'wb')
         f.write(etree.tostring(root, encoding='UTF-8', pretty_print=True))
         f.close()
 
-class SpreadSheet():
-    def __init__(self):
-        # count_of_keys = len(di)
-        #from openpyxl import Workbook, load_workbook
-        print("")
 
+class SpreadSheet:
+    def __init__(self, from_file, to_file):
+        self.from_file = from_file
+        self.to_file = to_file
 
     def exportXLSX(self):
-
-
         wb = Workbook()
         ws = wb.active
         # create a dict of the contents of the output XML file
 
-        doc = etree.parse(output_file)
-        di = dict()
+        doc = etree.parse(from_file)
+        '''A standard dict does not maintain insertion order thus we need to use collections.OrderedDict. If we do not,
+        the generated XLSX will have a mixed orderring of key/value pairs'''
+        di = OrderedDict()
         for n in doc.getroot().iterdescendants():
             tmp = n.attrib
+
             if tmp.get('key'):
                 di[tmp.get('key')] = n.text
                 # print(str(tmp.get('key')) + " --  "  + str(n.text))
+
+        # print(di)
         for index, (key, val) in enumerate(di.items()):
-            print (index, key, val)
+            # print (index, key, val)
+            # if key=='xmlui.general.dspace_home':
+            #     print (index)
             index=index+1
             colA = "A" + str(index)
             colB = "B" + str(index)
@@ -76,24 +87,88 @@ class SpreadSheet():
             ws[colA] = key
             ws[colB] = val
 
-        wb.save("export.xlsx")
+        wb.save(to_file)
 
     def importXLSX(self):
-        # content goes here
-        print("import")
 
-        wb = load_workbook('export.xlsx')
+        wb = load_workbook(exported_file)
         sheet = wb.get_sheet_by_name('Sheet')
         print(sheet.max_row)
+
+        # Parse the new XML translation
+        dest = etree.parse(output_file)
+        root = dest.getroot()
         num_of_keys = sheet.max_row
         for x in range(1, num_of_keys):
             colA = "A" + str(x)
             colB = "B" + str(x)
+            code = root.xpath("/catalogue/message[@key='" + sheet[colA].value + "']")
+            if code:
+                code[0].text = sheet[colB].value
 
             #print(sheet[colA].value + "  " + sheet[colB].value)
+        f = open(imported_file, 'wb')
+        f.write(etree.tostring(root, encoding='UTF-8', pretty_print=True))
+        f.close()
 
 if __name__ == "__main__":
-    XmluiParser()
-    # ss = SpreadSheet()
-    # ss.exportXLSX()
-    # ss.importXLSX()
+
+    parser = argparse.ArgumentParser(description='This is a script to assist in translating XMLUI.')
+    # parser.add_argument('--foo', action='store_true', help='foo help')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # create the parser for the "merge" command
+    parser_a = subparsers.add_parser('migrate', help='Migrate translation messages from an older version to the latest.')
+    parser_a.add_argument('-f', '--from-file', default=incomplete_translation_file,
+                        help='Read translation strings from this file.',
+                        required=False)
+    parser_a.add_argument('-t', '--to-file', default=output_file,
+                        help='Place translations in this file (this usually being the latest version).',
+                        required=False)
+    parser_a.set_defaults(which='migrate')
+
+    # create the parser for the "export" command
+    parser_b = subparsers.add_parser('export', help='Convert an XML translation file to XLSX for easier editing.')
+    parser_b.add_argument('-f', '--from-file', default=output_file,
+                        help='Read translation strings from this file.',
+                        required=False)
+    parser_b.add_argument('-t', '--to-file', default=exported_file,
+                        help='Place translations in this file (this usually being the latest version).',
+                        required=False)
+    parser_b.set_defaults(which='export')
+
+    # create the parser for the "import" command
+    parser_c = subparsers.add_parser('import', help='Convert an XLSX translation file to the native messages_xx.xml format.')
+    parser_c.add_argument('-f', '--from-file', default=exported_file,
+                        help='Read translation strings from this file.',
+                        required=False)
+    parser_c.add_argument('-t', '--to-file', default=imported_file,
+                        help='Place translations in this file (this usually being the latest version).',
+                        required=False)
+    parser_c.set_defaults(which='import')
+
+
+    args = parser.parse_args()
+    # print(args)
+    # print(args.which)
+    try:
+        from_file = args.from_file
+        to_file = args.to_file
+        if args.which == 'migrate':
+            # print("Migrate")
+            obj = XmluiParser(from_file, to_file)
+            obj.processFiles()
+
+        elif args.which == 'export':
+            # print("Export")
+            obj =  SpreadSheet(from_file, to_file)
+            obj.exportXLSX()
+        elif args.which == 'import':
+            # print("Import")
+            obj =  SpreadSheet(from_file, to_file)
+            obj.importXLSX()
+        else:
+            print ("Use -h for instructions.")
+    except AttributeError:
+        print ("Use -h for instructions.")
+
